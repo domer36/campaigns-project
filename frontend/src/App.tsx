89f1e5e41f061408db62c51f4fbbe06d963f3fd4
Carlos Domínguez <carlos.cfdr86@gmail.com>
2025-04-04 14:02:32 -0600

    Se protege ruta de dashboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,6 +8,7 @@ import "./App.css";
 import { AuthProvider } from "./core/auth/AuthContext";
 import LoginPage from "./pages/Login/LoginPage";
 import DashboardPage from "./pages/Dashboard/DashboardPage";
+import PrivateRoute from "./core/auth/PrivateRoute";
 
 function App() {
   return (
@@ -15,7 +16,9 @@ function App() {
       <AuthProvider>
         <Routes>
           <Route path="/login" element={<LoginPage />} />
-          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route element={<PrivateRoute />}>
+            <Route path="/dashboard" element={<DashboardPage />} />
+          </Route>
           <Route path="*" element={<Navigate to="/login" />} />
         </Routes>
       </AuthProvider>
