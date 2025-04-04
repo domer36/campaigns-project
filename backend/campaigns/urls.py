4723ea3d8aeaf6b31c3ca45953d3fbd1f9d93296
Carlos Domínguez <carlos.cfdr86@gmail.com>
2025-04-04 03:35:00 -0600

    Se integra endpoint dashboard para obtener un resumen sobre las campañas agrupandolas por estatus

--- backend/campaigns/urls.py
+++ backend/campaigns/urls.py
@@ -1,7 +1,11 @@
+from django.urls import path
+from .views import CampaignDashboardView
 from rest_framework.routers import DefaultRouter
 from .views import CampaignViewSet
 
 router = DefaultRouter()
 router.register(r'campaigns', CampaignViewSet, basename='campaign')
 
-urlpatterns = router.urls
+urlpatterns = router.urls + [
+    path('dashboard/', CampaignDashboardView.as_view(), name='campaign-dashboard'),
+]
--- backend/campaigns/views.py
+++ backend/campaigns/views.py
@@ -1,14 +1,52 @@
-from rest_framework import viewsets, permissions, filters
+from rest_framework import viewsets, permissions, filters, response, views
 from .models import Campaign
 from .serializers import CampaignSerializer
 from .permissions import IsSuperAdminOrReadOnly, IsOwnerOrSuperAdmin
 
+from django.db import models
+
 class CampaignViewSet(viewsets.ModelViewSet):
-    queryset = Campaign.objects.all()
     serializer_class = CampaignSerializer
     permission_classes = [IsSuperAdminOrReadOnly, IsOwnerOrSuperAdmin]
     filter_backends = [filters.SearchFilter]
-    search_fields = ['status']
+    filterset_fields  = ['status']
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.role == 'SUPERADMIN':
+            return Campaign.objects.all()
+        return Campaign.objects.filter(created_by=user)
 
     def perform_create(self, serializer):
         serializer.save(created_by=self.request.user)
+
+class CampaignDashboardView(views.APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+
+        # Obtiene todas las campañas si el rol es super admin
+        # Sino solo regresa las del admin
+        if user.role == 'SUPERADMIN':
+            campaigns = Campaign.objects.all()
+        else:
+            campaigns = Campaign.objects.filter(created_by=user)
+
+        total_campaigns = campaigns.count()
+        total_budget = campaigns.aggregate(models.Sum('budget'))['budget__sum'] or 0
+
+        campaigns_summary = {}
+        # Agrupamos las campañas por status
+        for status, _ in Campaign.Status.choices:
+            subset = campaigns.filter(status=status)
+            campaigns_summary[status] = {
+                'total': subset.count(), # Obtenemos el total de campañas por status
+                'budget': float(subset.aggregate(models.Sum('budget'))['budget__sum'] or 0) # Sumamos el presupuesto por status
+            }
+
+        return response.Response({
+            'total_campaigns': total_campaigns,
+            'total_budget': float(total_budget),
+            'campaigns': campaigns_summary
+        })
